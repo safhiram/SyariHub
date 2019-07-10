9bed1b940d1fb43fc2c28d5774a8890f81bdb954
Unknown <safhira.maharani14@gmail.com>
2019-07-10 16:04:50 +0700

    html home

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -8,92 +8,120 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="/css/welcome.css">
+        <!-- Boostrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <!-- SlideShow -->
+    <div id="slideshow">
+        <div class="slideshow-container">
+            <div class="mySlides">
+                <div class="numbertext">1 / 3</div>
+                <img src="/img/SYARI_RIDE.png" style="width:100%">
+                <div class="text">Caption Text</div>
+            </div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div class="mySlides">
+                <div class="numbertext">2 / 3</div>
+                <img src="/img/MOKUP_CATERING.png" style="width:100%">
+                <div class="text">Caption Two</div>
+            </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div class="mySlides">
+                <div class="numbertext">3 / 3</div>
+                <img src="/img/MOKUP_NANNY.png" style="width:100%">
+                <div class="text">Caption Three</div>
             </div>
         </div>
+        <div style="text-align:center">
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+        </div>
+    </div>
+    <div id="pengenalan">
+        <div class="row">
+            <div class="col-sm-6">
+                Tulisan Pengenalan
+            </div>
+            <div class="col-sm-6"> 
+                Video
+            </div>
+        </div>
+    </div>
+    <div id="macam_layanan">
+        <div id="tulisan">
+            <h4>Dengan Layanan Kami, Kebutuhan Anda Terpenuhi</h4>
+        </div>
+        <div id="atas" class="row">
+            <div class="col-sm-2">1</div>
+            <div class="col-sm-2">2</div>
+            <div class="col-sm-2">3</div>
+            <div class="col-sm-2">4</div>
+            <div class="col-sm-2">5</div>
+            <div class="col-sm-2">6</div>          
+        </div>
+        <div id="bawah" class="row">
+            <div class="col-sm-2">1</div>
+            <div class="col-sm-2">2</div>
+            <div class="col-sm-2">3</div>
+            <div class="col-sm-2">4</div>
+            <div class="col-sm-2">5</div>
+            <div class="col-sm-2">6</div> 
+        </div>
+    </div>
+    <div id="download">
+        <div id="tulisan">
+            Download Sekarang di..
+        </div>
+        <div id="icon_gogleplay">
+            gambar
+        </div>
+    </div>
+    <div id="telah_diliput">
+        <div id="tulisan">
+            Telah Diliput Oleh
+        </div>
+        <div id="icon_media" class="row">
+            <div class="col-sm-4">
+                gambar1
+            </div> 
+            <div class="col-sm-4">
+                gambar2
+            </div>
+            <div class="col-sm-4">
+                gambar3
+            </div>                
+        </div>
+    </div>
+    <div id="footer">
+        
+    </div>
+    <script>
+        var slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+        }
+    </script>
     </body>
 </html>
