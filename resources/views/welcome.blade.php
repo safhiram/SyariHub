2598b5d79b753dbf9d788588c9d4b808d875b4f5
Unknown <safhira.maharani14@gmail.com>
2019-07-23 15:52:45 +0700

    kembali ke semula

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -238,7 +238,6 @@
     <script>
     var slideIndex = 0;
     showSlides();
-
     function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -263,24 +262,18 @@
         this.tick();
         this.isDeleting = false;
     };
-
     TxtType.prototype.tick = function() {
         var i = this.loopNum % this.toRotate.length;
         var fullTxt = this.toRotate[i];
-
         if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
         } else {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
-
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
         var that = this;
         var delta = 200 - Math.random() * 100;
-
         if (this.isDeleting) { delta /= 2; }
-
         if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
@@ -289,12 +282,10 @@
         this.loopNum++;
         delta = 500;
         }
-
         setTimeout(function() {
         that.tick();
         }, delta);
     };
-
     window.onload = function() {
         var elements = document.getElementsByClassName('typewrite');
         for (var i=0; i<elements.length; i++) {
