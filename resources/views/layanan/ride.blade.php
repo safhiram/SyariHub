bc88e72d64cb1b4f7ba677deacc5beb171eeeaf9
Unknown <safhira.maharani14@gmail.com>
2019-07-30 14:44:44 +0700

    halaman layanan ride, ridebln,cat

--- resources/views/layanan/ride.blade.php
+++ resources/views/layanan/ride.blade.php
@@ -9,11 +9,12 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="/css/welcome.css">
+        <link rel="stylesheet" type="text/css" href="/css/mitra.css">
+        <link rel="stylesheet" type="text/css" href="/css/layanan.css">
         <!-- Boostrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,500i,700,800i" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-        <link rel="stylesheet" href="https://bilba.go-jek.com/v4/css/gojek/main.css">
         <!-- Javascript -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -71,64 +72,216 @@
                 </div>
         </nav>
         <!-- Mulai Ngoding -->
-        <section class="c-section">
+        <section class="c-section" >
             <div class="c-hero c-hero--banner-full c-hero--font-2">
-                <div class="row js-slider-both" data-slider-desktop="true,false,1,true,true,true"
-                    data-slider-mobile="true,false,1,true,true,true">
-                    
-                    
-                    <div class="col-12">
-                        <div class="c-hero--banner-full__wrapper container-fluid">
-                            <picture> 
-                            <source src="/img/header_layanan/ride1.png" srcset="/img/header_layanan/ride1.png 400w, /img/header_layanan/ride1.png 800w, /img/header_layanan/ride1.png 3000w" media="(min-width: 769px)" alt="Syarihub-RIDE" type="image/webp">
-                            <source src="/img/header_layanan/ride2.png" srcset="/img/header_layanan/ride2.png 400w, /img/header_layanan/ride2.png 800w, /img/header_layanan/ride2.png 3000w" media="(min-width: 769px)" alt="Syarihub-RIDE" type="image/jpeg">
-                            <img src="/img/header_layanan/ride1.png" srcset="/img/header_layanan/ride1.png 400w, /img/header_layanan/ride1.png 800w, /img/header_layanan/ride1.png 3000w"alt="Syarihub-RIDE" type="image/webp">
-                            <img src="/img/header_layanan/ride2.png" srcset="/img/header_layanan/ride2.png 400w, /img/header_layanan/ride2.png 800w, /img/header_layanan/ride2.png 3000w" alt="Syarihub-RIDE" type="image/jpeg">
-                            </picture>
+                <div class="container-fluid"style="padding-left:0px;padding-right:0px;">
+                    <div class="row js-slider-both" data-slider-desktop="true,false,1,true,true,true"
+                        data-slider-mobile="true,false,1,true,true,true">
+                        
+                        <div class="col-12">
+                            <div class="c-hero--banner-full__wrapper container-fluid">
+                                <picture> 
+                                <source src="/img/header_layanan/ride1.png" srcset="/img/header_layanan/ride1.png 400w, /img/header_layanan/ride1.png 800w, /img/header_layanan/ride1.png 3000w" media="(min-width: 769px)" alt="Syarihub-RIDE" type="image/webp">
+                                <source src="/img/header_layanan/ride2.png" srcset="/img/header_layanan/ride2.png 400w, /img/header_layanan/ride2.png 800w, /img/header_layanan/ride2.png 3000w" media="(min-width: 769px)" alt="Syarihub-RIDE" type="image/jpeg">
+                                <img src="/img/header_layanan/ride1.png" srcset="/img/header_layanan/ride1.png 400w, /img/header_layanan/ride1.png 800w, /img/header_layanan/ride1.png 3000w"alt="Syarihub-RIDE" type="image/webp">
+                                <img src="/img/header_layanan/ride2.png" srcset="/img/header_layanan/ride2.png 400w, /img/header_layanan/ride2.png 800w, /img/header_layanan/ride2.png 3000w" alt="Syarihub-RIDE" type="image/jpeg">
+                                </picture>
 
 
-                            <div class="container">
-                                <div class="c-hero--banner-full__content u-text-align__center--mobile u-text-align__center--tablet">
-                                    <div class="row">
-                                        <div class="col-12 col-md-6">
-                                            <h1>SyariHub-RIDE</h1>
-                                            
-                                            <p style="color:grey;font-family:'Lucida Sans';font-size: 120%;">
-                                            <strong> Syarihub menyediakan layanan <br>untuk Anda para wanita dan anak <br>untuk mendapat fasilitas antar <br>jemput yang Aman dan Nyaman</strong>
-                                            </p>      
-                                            <div class="c-hero--banner-full__content__action mt-2 mt-md-4">
-                                                <a href="javascript:;">
-                                                    <button type="button" class="js-show-popup"
-                                                        data-id="content-video-hero-2">
-                                                        Lihat Lebih
-                                                        
-                                                    </button>
-                                                </a>
-                                            </div>
-
-                                                <div class="u-hide--all" id="content-video-hero-2">
-                                                    <div class="embed-responsive embed-responsive-21by9">
-                                                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/viUwhsB00i8?autoplay=1&amp;enablejsapi=1&amp;rel=0?rel=0"
-                                                            frameborder="0"
-                                                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                                            allowfullscreen>
-                                                        </iframe>
-                                                    </div>
+                                <div class="container">
+                                    <div class="c-hero--banner-full__content u-text-align__center--mobile u-text-align__center--tablet">
+                                        <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <h1>SyariHub-RIDE</h1>
+                                                
+                                                <p style="color:grey;font-family:'Lucida Sans';font-size: 120%;">
+                                                <strong> Syarihub menyediakan layanan <br>untuk Anda para wanita dan anak <br>untuk mendapat fasilitas antar <br>jemput yang Aman dan Nyaman</strong>
+                                                </p>      
+                                                <div class="c-hero--banner-full__content__action mt-2 mt-md-4">
+                                                    <a href="javascript:;">
+                                                        <button type="button" class="js-show-popup"
+                                                            data-id="content-video-hero-2">
+                                                            Lihat Lebih
+                                                            
+                                                        </button>
+                                                    </a>
                                                 </div>
-                                            
+
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
+                        </div>
+                        
+                    </div>
+                </div>  
+            </div>
+        </section>
+        <div >
+        <div class="container-fluid" id="satu">
+            <div id="tulisan">
+            <b>MENGAPA SYARIHUB RIDE?</b>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-sm-1">
+                </div>
+                <div class="col-sm-2" id="keuntungan">
+                    <div>
+                        <img src="/img/duit.png">
+                    </div>
+                    <div>
+                        <br>Menambah penghasilan sehari hari
+                    </div>
+                </div>
+                <div class="col-sm-2" id="keuntungan">
+                    <div>
+                        <img src="/img/duit.png">
+                    </div>
+                    <div>
+                        <br>Menambah penghasilan sehari hari
+                    </div>
+                </div>
+                <div class="col-sm-2" id="keuntungan">
+                    <div>
+                        <img src="/img/duit.png">
+                    </div>
+                    <div>
+                        <br>Menambah penghasilan sehari hari
+                    </div>
+                </div>
+                <div class="col-sm-2" id="keuntungan">
+                    <div>
+                        <img src="/img/duit.png">
+                    </div>
+                    <div>
+                        <br>Menambah penghasilan sehari hari
+                    </div>
+                </div>
+                <div class="col-sm-2" id="keuntungan">
+                    <div>
+                        <img src="/img/duit.png">
+                    </div>
+                    <div>
+                        <br>Menambah penghasilan sehari hari
+                    </div>
+                </div>
+                <div class="col-sm-1">
+                </div>
+            </div>  
+        </div><br><br><br>
+        <div class="container" id="dua">
+            <div id="tulisan">
+            <b>TESTIMONI CUSTOMER RIDE</b>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-3">
+                </div>
+                <div class="col-lg-6 col-sm-6">
+
+                    <div class="card hovercard">
+                        <div class="cardheader">
+                        </div>
+                        <div class="avatar">
+                            <a><img alt="" src="/img/profil.png"></a>
+                        </div>
+                        <div class="info">
+                            <div class="title">
+                                <a target="_blank" href="http://www.doweb.in/">Heni A. - Sidoarjo</a>
+                            </div>
+                            <div class="desc">Sejak bergabung menjadi Sahabat Penendara SyariHub Ride <br> saya mulai berhijab dan mengikuti pengajian rutin RT.</div>
+                            <div class="desc">Anak juga suka dengan perkembangan tersebut.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                </div>
+            </div>
+        </div><br><br><br>
+        <div class="container" id="tiga">
+            <div id="tulisan">
+            <b>CARA PAKAI SYARIHUB-RIDE</b>
+            </div>
+            
+            <div class="section" id="carousel">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 mr-auto ml-auto">
+
+                            <!-- Carousel Card -->
+                            <div class="card card-raised card-carousel">
+                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="3" class=""></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                    <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg.jpg"
+                                    alt="First slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg2.jpg"  alt="Second slide">
+                    
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg3.jpg" alt="Third slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg2.jpg"  alt="Second slide">
+                    
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <i class="material-icons"><</i>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <i class="material-icons">></i>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                                </div>
+                            </div>
+                            <!-- End Carousel Card -->
 
                         </div>
                     </div>
-                    
                 </div>
             </div>
-        </section>
-        
+    </div>
         <!-- Selesai Ngoding -->
+        <div id="footer" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-5">
+            </div>
+            <div class="col-sm-2">
+                <div class="row">
+                    <div class="yap">
+                        <a href="https://www.youtube.com/channel/UCviHmF9zS25B19n6lYarVJw">
+                            <img src="/img/youtube.png" style="width:120%;max-width:40px;">
+                        </a>
+                    </div>
+                    <div class="yap">
+                        <a href="https://www.facebook.com/Admin1.Ojesy">
+                            <img src="/img/facebook.png" style="width:100%;max-width:40px;">
+                        </a>
+                    </div>
+                    <div class="yap">
+                        <a href="https://www.instagram.com/syarihub.id/">
+                            <img src="/img/instagram.png" style="width:100%;max-width:40px;">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-5">
+            </div>
+        </div>
+    </div>
         <script type="text/javascript">
             $(document).ready(function () {
             $('.navbar-light .dmenu').hover(function () {
