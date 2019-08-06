8b056abef57d99b70f7f11ae5d438ae8ede8b156
Safhira Maharani <37364680+safhiram@users.noreply.github.com>
2019-08-06 12:18:27 +0700

    Update 2019_07_28_085043_create_massages_table.php

--- database/migrations/2019_07_28_085043_create_massages_table.php
+++ database/migrations/2019_07_28_085043_create_massages_table.php
@@ -16,7 +16,21 @@ class CreateMassagesTable extends Migration
         Schema::create('massages', function (Blueprint $table) {
             $table->bigIncrements('id');
             //isi dari database apa aja
-
+            $table->string('nama_lengkap');
+            $table->string('tempat_lahir');
+            $table->string('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->string('alamat_ktp');
+            $table->string('alamat_tinggal');
+            $table->string('no_ktp');
+            $table->string('pendidikan');
+            $table->string('hp_android');
+            $table->string('keahlian');
+            $table->string('status');
+            $table->string('pengalaman');
+            $table->string('darimana');
+            $table->string('alasan');
+            $table->string('jam_kerja');
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
         });
