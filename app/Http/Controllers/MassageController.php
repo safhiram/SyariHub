7640cf1bd5e7d8fcce75540ab9da6708e807725d
Safhira Maharani <37364680+safhiram@users.noreply.github.com>
2019-08-06 12:17:40 +0700

    Update MassageController.php

--- app/Http/Controllers/MassageController.php
+++ app/Http/Controllers/MassageController.php
@@ -8,11 +8,27 @@ class MassageController extends Controller
 {
     public function index()
     {
-        return view('mitra-massage');
+        return view('mitra.massage');
     }
-    public function store()
+    public function store(Request $request)
     {
-        //yang mau dimasukin
+        $massage= new Massage;
+        $massage->nama_lengkap=request('nama_lengkap');
+        $massage->tempat_lahir=request('tempat_lahir');
+        $massage->tanggal_lahir=$request->tanggal_lahir;
+        $massage->jenis_kelamin=request('jenis_kelamin');
+        $massage->alamat_ktp=request('alamat_ktp');
+        $massage->alamat_tinggal=request('alamat_tinggal');
+        $massage->no_ktp=request('no_ktp');
+        $massage->pendidikan=request('pendidikan');
+        $massage->hp_android=request('hp_android');
+        $massage->keahlian=request('keahlian');
+        $massage->status=request('status');
+        $massage->pengalaman=request('pengalaman');
+        $massage->darimana=request('darimana');
+        $massage->alasan=request('alasan');
+        $massage->jam_kerja=request('jam_kerja');
+        $massage->save();
         return redirect()->back();
     }
 }
