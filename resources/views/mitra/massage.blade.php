6fe8b857149cf402c47591b1ba1bd7a71c48980b
Safhira Maharani <37364680+safhiram@users.noreply.github.com>
2019-08-06 12:19:19 +0700

    Update massage.blade.php

--- resources/views/mitra/massage.blade.php
+++ resources/views/mitra/massage.blade.php
@@ -8,6 +8,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="/css/form.css">
+        <link rel="stylesheet" type="text/css" href="/css/mitra.css">
         <link rel="stylesheet" type="text/css" href="/css/welcome.css">
         <!-- Boostrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -16,7 +18,8 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,500i,700,800i" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+
     </head>
     <body>
     <!--============================= Navbar =============================-->
@@ -37,14 +40,13 @@
                 Layanan
                 </a>
                 <div class="dropdown-menu sm-menu">
-                <a class="dropdown-item" href="#"><img src="/img/icon/ojek.png">   Syari Ride</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/ojek-bln.png">   Syari Ride Bulanan</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/nany.png">   Syari Nanny</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/ngaji.png">   Syari Ngaji</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/catering.png">   Syari Catering</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/massage.png">   Syari Massage</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/store.png">   Syari Store</a>
-                <a class="dropdown-item" href="#"><img src="/img/icon/lifestyle.png">   Syari Lifestyle</a>
+                <a class="dropdown-item" href="ride"><img src="/img/icon/ojek.png">   SyariHub Ride</a>
+                        <a class="dropdown-item" href="ride-bulanan"><img src="/img/icon/ojek-bln.png">   SyariHub Ride Bulanan</a>
+                        <a class="dropdown-item" href="nanni"><img src="/img/icon/nany.png">   SyariHub Nanny</a>
+                        <a class="dropdown-item" href="ngaji"><img src="/img/icon/ngaji.png">   SyariHub Al-Quran Mentor</a>
+                        <a class="dropdown-item" href="catering"><img src="/img/icon/catering.png">   SyariHub Catering</a>
+                        <a class="dropdown-item" href="massage"><img src="/img/icon/massage.png">   SyariHub Massage</a>
+                        <a class="dropdown-item" href="store"><img src="/img/icon/store.png">   SyariHub Store</a>
             </div>
             </li>
             <li class="nav-item">
@@ -63,43 +65,192 @@
             <a class="dropdown-item" href="/mitra-pengendara"><img src="/img/icon/ojek.png">   Pengendara</a>
               <a class="dropdown-item" href="/mitra-nanny"><img src="/img/icon/nany.png">   Nanny</a>
               <a class="dropdown-item" href="/mitra-mentor"><img src="/img/icon/ngaji.png">   Mentor Al-Quran</a>
+              <a class="dropdown-item" href="/mitra-massage"><img src="/img/icon/massage.png">   Massage</a>
             </div>
           </li>
         </ul>
         </div>
 </nav>
+
+<!--Mulai Ngoding isi page di sini -->
+<br>
+<div class="container">
+        <div id="tulisan">
+                <b>FORM PENDAFTARAN SYARIHUB MASSAGE</b>
+        </div>
+        <br>
+    <div class="stepwizard">
+        <div class="stepwizard-row setup-panel">
+            <div class="stepwizard-step">
+                <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
+                <p>Step 1</p>
+            </div>
+            <div class="stepwizard-step">
+                <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+                <p>Step 2</p>
+            </div>
+            <div class="stepwizard-step">
+                <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+                <p>Step 3</p>
+            </div>
+        </div>
+    </div>
+    
+    <form role="form" action="" method="post" autocomplete="off">
+        @csrf
+        <div class="setup-content" id="step-1">
+            <div class="col-xs-12">
+                <div class="col-sm-12">
+                    <h3> Step 1</h3>
+                    <div class="form-group">
+                        <label class="control-label">Nama Lengkap</label>
+                        <input maxlength="100" type="text" name="nama_lengkap" id="nama_lengkap" required="required" class="form-control" placeholder="Nama Lengkap" />
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Tempat Lahir</label>
+                        <input maxlength="100" type="text" name="tempat_lahir" id="tempat_lahir" required="required" class="form-control" placeholder="Tempat Lahir" />
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Tanggal Lahir</label>
+                        <input type="date" name="tanggal_lahir" id="tanggal_lahir" required="required" class="form-control" placeholder="Tanggal Lahir" />
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Jenis Kelamin</label>
+                        <select class="form-control" name="jenis_kelamin" id="jenis_kelamin">
+                                                <option value="null"></option>
+                                                <option value="Wanita">Wanita</option>
+                                                <option value="Pria">Pria</option>
+                          </select>
+                    </div>
+                    <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+            </div></div>
+        </div>
+        <div class="setup-content" id="step-2">
+            <div class="col-xs-12">
+                <div class="col-md-12">
+                    <h3> Step 2</h3>
+                    <div class="form-group">
+                        <label class="control-label">Alamat Sesuai KTP</label>
+                        <input maxlength="100" type="text" name="alamat_ktp" id="alamat_ktp" required="required" class="form-control" placeholder="Alamat Sesuai KTP" />
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Alamat Tinggal</label>
+                        <input maxlength="100" type="text" name="alamat_tinggal" id="alamat_tinggal" required="required" class="form-control" placeholder="Alamat Tinggal" />
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">No. KTP</label>
+                        <input maxlength="100" type="text" name="no_ktp" id="no_ktp" required="required" class="form-control" placeholder="Nomor KTP" />
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Pendidikan Terakhir</label>
+                        <select class="form-control" name="pendidikan" id="pendidikan">
+                                                <option value="null"></option>
+                                                <option value="S1">S1</option>
+                                                <option value="D3">D3</option>
+                                                <option value="SMA">SMA</option>
+                                                <option value="SMP">SMP</option>
+                                                <option value="SD">SD</option>
+                          </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Memiliki HP Android?</label><br>
+                        <input type="radio" id="hp_android"  name="hp_android" value="Ya">Ya <br>
+                        <input type="radio" id="hp_android"  name="hp_android" value="Tidak"> Tidak
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Keahlian Memijat</label>
+                        <select class="form-control" name="keahlian" id="keahlian">
+                                                <option value="null"></option>
+                                                <option value="Pijat Ibu dan Anak">Pijat Ibu dan Anak</option>
+                                                <option value="Pijat Pra/Pasca Kelahiran">Pijat Pra/Pasca Kelahiran</option>
+                                                <option value="Pijat Refleksi">Pijat Refleksi</option>
+                          </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Status</label>
+                        <select class="form-control" name="status" id="status">
+                                                <option value="null"></option>
+                                                <option value="Menikah">Menikah</option>
+                                                <option value="Belum Menikah">Belum Menikah</option>
+                                                <option value="Pernah Menikah">Pernah Menikah</option>
+                          </select>
+                    </div>
+                    
+                    <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+                </div>
+            </div>
+        </div>
+        <div class="setup-content" id="step-3">
+            <div class="col-xs-12">
+                <div class="col-md-12">
+                    <h3> Step 3</h3>
+                    <div class="form-group">
+                        <label class="control-label">Berapa tahun pengalaman memijat?</label>
+                        <select class="form-control" name="pengalaman" id="pengalaman">
+                                                <option value="null"></option>
+                                                <option value="kurang dari 1 tahun">Kurang dari 1 tahun</option>
+                                                <option value="1 - 2 tahun">1 - 2 tahun</option>
+                                                <option value="2 - 3 tahun">2 - 3 tahun</option>
+                                                <option value="Lebih dari 3 tahun">Lebih dari 3 tahun</option>
+                          </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Darimana kamu mengetahui SyariHub Massage?</label>
+                                            <select class="form-control" name="darimana" id="darimana">
+                                                <option value="null"></option>
+                                                <option value="Website">Website</option>
+                                                <option value="Media_Cetak">Media Cetak</option>
+                                                <option value="Media_Sosial">Media Sosial</option>
+                                                <option value="Teman">Teman</option>
+                                                <option value="Lainnya">Lainnya</option>
+                                            </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Alasan bergabung dengan SyariHub Massage?</label>
+                        <select class="form-control" name="alasan" id="alasan">
+                                                <option value="null"></option>
+                                                <option value="Waktu kerja yang flexibel">Waktu kerja yang flexibel</option>
+                                                <option value="Pendapatan yang menjanjikan">Pendapatan yang menjanjikan</option>
+                                                <option value="Bidang pekerjaan yang sesuai minat">Bidang pekerjaan yang sesuai minat</option>
+                                                <option value="Lainnya">Lainnya</option>
+                          </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Waktu yang dialokasikan untuk menjadi driver?</label><br>
+                        <input type="radio" id="jam_kerja"  name="jam_kerja" value="Full_Time">Full Time <br>
+                        <input type="radio" id="jam_kerja"  name="jam_kerja" value="Part_Time"> Part Time
+                    </div>
+                    
+                    <button class="btn btn-success btn-lg pull-right" type="submit">Finish!</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
 <br><br><br>
-<div id="kedua">
+<div id="pertama">
 <div class="container-fluid">
     <div id="tulisan">
-    <b>MENGAPA SYARIHUB RIDE?</b>
+    <b>MENGAPA SYARIHUB MASSAGE?</b>
     </div>
-    <br><br>
+    <br><br><br>
     <div class="row">
-        <div class="col-sm-1">
+        <div class="col-sm-3">
         </div>
         <div class="col-sm-2" id="keuntungan">
             <div>
-                <img src="/img/duit.png">
+                <img src="/img/syarat.png">
             </div>
             <div>
-                <br>Menambah penghasilan sehari hari
-            </div>
-        </div>
-        <div class="col-sm-2" id="keuntungan">
-            <div>
-                <img src="/img/duit.png">
-            </div>
-            <div>
-                <br>Menambah penghasilan sehari hari
+                <br>Persyaratan Mudah
             </div>
         </div>
         <div class="col-sm-2" id="keuntungan">
             <div>
-                <img src="/img/duit.png">
+                <img src="/img/time.png">
             </div>
             <div>
-                <br>Menambah penghasilan sehari hari
+                <br>Jam Kerja Fleksibel
             </div>
         </div>
         <div class="col-sm-2" id="keuntungan">
@@ -107,52 +258,14 @@
                 <img src="/img/duit.png">
             </div>
             <div>
-                <br>Menambah penghasilan sehari hari
+                <br>Penghasilan Berkah
             </div>
         </div>
-        <div class="col-sm-2" id="keuntungan">
-            <div>
-                <img src="/img/duit.png">
-            </div>
-            <div>
-                <br>Menambah penghasilan sehari hari
-            </div>
-        </div>
-        <div class="col-sm-1">
+        <div class="col-sm-3">
         </div>
     </div>
 </div>
-<br><br><br>
-<div class="container">
-    <div id="tulisan">
-    <b>TESTIMONI PENGENDARA</b>
-    </div>
-    <br>
-    <div class="row">
-        <div class="col-lg-3">
-        </div>
-		<div class="col-lg-6 col-sm-6">
-
-            <div class="card hovercard">
-                <div class="cardheader" style="background-color:white;">
-                </div>
-                <div class="avatar">
-                    <a><img alt="" src="/img/profil.png"></a>
-                </div>
-                <div class="info">
-                    <div class="title">
-                        <a target="_blank" href="http://www.doweb.in/">Heni A. - Sidoarjo</a>
-                    </div>
-                    <div class="desc">Sejak bergabung menjadi Sahabat Penendara SyariHub Ride <br> saya mulai berhijab dan mengikuti pengajian rutin RT.</div>
-                    <div class="desc">Anak juga suka dengan perkembangan tersebut.</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-        </div>
-    </div>
-</div>
-</div>
+<br>
 <br><br>
 <div class="container-fluid">
     <div id="tulisan">
@@ -194,9 +307,34 @@
         </div>
     </div>
 </div>
-
-<!--Mulai Ngoding isi page di sini -->
-
+<!--Selesai Isi Page -->
+<div id="footer" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-5">
+            </div>
+            <div class="col-sm-2">
+                <div class="row">
+                    <div class="yap">
+                        <a href="https://www.youtube.com/channel/UCviHmF9zS25B19n6lYarVJw">
+                            <img src="/img/youtube.png" style="width:120%;max-width:40px;">
+                        </a>
+                    </div>
+                    <div class="yap">
+                        <a href="https://www.facebook.com/Admin1.Ojesy">
+                            <img src="/img/facebook.png" style="width:100%;max-width:40px;">
+                        </a>
+                    </div>
+                    <div class="yap">
+                        <a href="https://www.instagram.com/syarihub.id/">
+                            <img src="/img/instagram.png" style="width:100%;max-width:40px;">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-5">
+            </div>
+        </div>
+    </div>
 <script type="text/javascript">
     $(document).ready(function () {
     $('.navbar-light .dmenu').hover(function () {
@@ -205,6 +343,52 @@
             $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
         });
     });
-    </script>
+</script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+
+var navListItems = $('div.setup-panel div a'),
+        allWells = $('.setup-content'),
+        allNextBtn = $('.nextBtn');
+
+allWells.hide();
+
+navListItems.click(function (e) {
+    e.preventDefault();
+    var $target = $($(this).attr('href')),
+            $item = $(this);
+
+    if (!$item.hasClass('disabled')) {
+        navListItems.removeClass('btn-primary').addClass('btn-default');
+        $item.addClass('btn-primary');
+        allWells.hide();
+        $target.show();
+        $target.find('input:eq(0)').focus();
+    }
+});
+
+allNextBtn.click(function(){
+    var curStep = $(this).closest(".setup-content"),
+        curStepBtn = curStep.attr("id"),
+        nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+        curInputs = curStep.find("input[type='text'],input[type='url']"),
+        isValid = true;
+
+    $(".form-group").removeClass("has-error");
+    for(var i=0; i<curInputs.length; i++){
+        if (!curInputs[i].validity.valid){
+            isValid = false;
+            $(curInputs[i]).closest(".form-group").addClass("has-error");
+        }
+    }
+
+    if (isValid)
+        nextStepWizard.removeAttr('disabled').trigger('click');
+});
+
+$('div.setup-panel div a.btn-primary').trigger('click');
+});
+</script>
     </body>
 </html>
